b608b5c0999a7593874f02e0d407838b8a1d0882
Nicolai Ommer <nicolai.ommer@qaware.de>
2018-06-16 22:31:39 -0400

    pass server-config to status-board server

--- start-match.sh
+++ start-match.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 terminator -e "cd ~/git/ssl-refbox && ./sslrefbox; read" &
-terminator -e "~/go/bin/ssl-status-board-server; read" &
+terminator -e "~/go/bin/ssl-status-board-server -c ~/server-config.yaml; read" &
 terminator -e "mkdir -p ~/logs; ~/git/ssl-logtools/build/bin/logrecorder --compress --output ~/logs/`date +%Y-%m-%d_%H-%M-%S.log.gz`; read" &
