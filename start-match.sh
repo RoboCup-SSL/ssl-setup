cc915db5a1949515bf4e6b4231344a7b0ec68e48
Nicolai Ommer <nicolai.ommer@qaware.de>
2018-06-17 09:21:29 -0400

    Use absolute path to ssl-recorder

--- start-match.sh
+++ start-match.sh
@@ -4,6 +4,6 @@ terminator -e "cd ~/git/ssl-refbox && ./sslrefbox; read" &
 sleep 3s
 terminator -e "~/go/bin/ssl-status-board-server -c ~/server-config.yaml; read" &
 #terminator -e "mkdir -p ~/logs; ~/git/ssl-logtools/build/bin/logrecorder --compress --output ~/logs/`date +%Y-%m-%d_%H-%M-%S.log.gz`; read" &
-terminator -e "mkdir -p ~/logs; ssl-recorder; read" &
+terminator -e "mkdir -p ~/logs; ~/go/bin/ssl-recorder; read" &
 terminator -e "cd ~/git/ssl-autorefs && ./run_tigers.sh; read" &
 terminator -e "cd ~/git/ssl-autorefs && ./run_erforce.sh; read" &
