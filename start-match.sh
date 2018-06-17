8e285fc0fd13e9d146b234befa34279c6bd05306
Nicolai Ommer <nicolai.ommer@qaware.de>
2018-06-16 22:20:42 -0400

    Update start script

--- start-match.sh
+++ start-match.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 terminator -e "cd ~/git/ssl-refbox && ./sslrefbox; read" &
-terminator -e "ssl-status-board-server; read" &
-terminator -e "~/git/ssl-logtools/build/bin/logrecorder; read" &
+terminator -e "~/go/bin/ssl-status-board-server; read" &
+terminator -e "mkdir -p ~/logs; ~/git/ssl-logtools/build/bin/logrecorder --compress --output ~/logs/`date +%Y-%m-%d_%H-%M-%S.log.gz`; read" &
