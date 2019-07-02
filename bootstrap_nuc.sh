47383e61e64ca3f770dd53b25619f951f4aa3b6e
Nicolai Ommer <nicolai.ommer@gmail.com>
2019-07-02 11:09:35 +1000

    Add bootstrap script for nuc

---
+++ bootstrap_nuc.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# This script will bootstrap a fresh Ubuntu 18.04 LTS installation with the latest ssl software
+# You will have to confirm some steps manually, as this script has no control over some installation scripts
+
+# exit on failures
+set -e
+# print commands
+set -x
+
+# references
+# https://github.com/Robocup-SSL
+
+# update system
+sudo apt -y update
+sudo apt -y dist-upgrade
+
+# install some common dependencies
+sudo apt install -y vim terminator git chrony net-tools
+
+# ssl-vision
+mkdir -p ~/git
+cd ~/git
+if [ ! -d ssl-vision ]; then
+	git clone http://github.com/RoboCup-SSL/ssl-vision.git
+fi
+cd ssl-vision
+git pull
+./InstallPackagesUbuntu.sh
+make -j`nproc`
+
+# setting up chrony
+# accept all IPv4 connections
+sudo sed -ie 's/#allow 0\/0/allow 0\/0/' /etc/chrony/chrony.conf
+# server time even if unsynchronized
+sudo sed -ie 's/#local stratum/local stratum/' /etc/chrony/chrony.conf
+sudo systemctl restart chrony.service
+echo "TODO you have to select one computer as the chrony server and add the IP to all other computers in /etc/chrony/chrony.conf as 'server 192.168.178.51'"
+read
+
