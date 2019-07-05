a70aff9f342b51b5e38989381d162f830398ba74
Devin Schwab <digidevin@gmail.com>
2019-07-05 14:23:22 +1000

    :bug: Stopping game controller before update, restarting after

--- robocup-2019/update-game-controller.sh
+++ robocup-2019/update-game-controller.sh
@@ -2,6 +2,7 @@
 
 set -eu
 
+systemctl --user stop ssl-game-controller.service
 BINARY_URL=$(curl -SsL https://github.com/RoboCup-SSL/ssl-game-controller/releases/latest \
 		 | grep 'ssl-game-controller_v.*_linux_amd64' \
 		 | awk '{$1=$1;print}' \
@@ -12,3 +13,4 @@ BINARY_URL=$(curl -SsL https://github.com/RoboCup-SSL/ssl-game-controller/releas
 
 curl -sSL $BINARY_URL > ~/.local/bin/ssl-game-controller
 chmod +x ~/.local/bin/ssl-game-controller
+systemctl --user start ssl-game-controller.service
