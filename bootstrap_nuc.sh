a904c823b21daface00a52c56760b3cfab12f89e
Nicolai Ommer <nicolai.ommer@gmail.com>
2019-11-24 20:20:04 +0100

    Add QT5 package

--- bootstrap_nuc.sh
+++ bootstrap_nuc.sh
@@ -20,7 +20,7 @@ sudo apt -y update
 sudo apt -y dist-upgrade
 
 # install some common dependencies
-sudo apt install -y vim terminator git chrony net-tools vnc4server openssh-server xserver-xorg-video-dummy-hwe-18.04
+sudo apt install -y vim terminator git chrony net-tools vnc4server openssh-server xserver-xorg-video-dummy-hwe-18.04 qt5-default
 
 sudo cp xorg.conf /usr/share/X11/xorg.conf.d/xorg.conf
 
